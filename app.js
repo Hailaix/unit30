099f3b9eda3d4d18c8e96cf6fe69edce6ebf7630
NHower <howernickoli@gmail.com>
2022-09-28 19:29:14 -0400

    operations complete, needs tests

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
 const express = require('express');
-
+const { mean, median, mode } = require('./operations');
 const app = express();
 
 app.get('/mean', function (req, res, next) {
     // return mean
     const ret = { operation: "mean" };
-    if (req.query.nums) {
-        let nums = req.query.nums.split(',');
-        nums = nums.map(Number);
-        try{
-            //mean(nums)
-        }
-        catch(e){
-            next(e);
-        }
-        ret.nums = nums;
+    let nums = req.query.nums.split(',');
+    nums = nums.map(Number);
+    try {
+        ret.value = mean(nums);
+    }
+    catch (e) {
+        next(e);
     }
     return res.json(ret);
 });
 
 app.get('/median', function (req, res, next) {
     //return median
-    if (req.query.nums) {
-        const ret = { operation: "median" };
-        let nums = req.query.nums.split(',');
-        nums = nums.map(Number);
-        try{
-            //median(nums)
-        }
-        catch(e){
-            next(e);
-        }
-        return res.json(ret);
+    const ret = { operation: "median" };
+    let nums = req.query.nums.split(',');
+    nums = nums.map(Number);
+    try {
+        ret.value = median(nums);
     }
+    catch (e) {
+        next(e);
+    }
+    return res.json(ret);
 });
 
 app.get('/mode', function (req, res, next) {
     //return mode
-    if (req.query.nums) {
-        const ret = { operation: "mode" };
-        let nums = req.query.nums.split(',');
-        nums = nums.map(Number);
-        try{
-            //mode(nums)
-        }
-        catch(e){
-            next(e);
-        }
-        return res.json(ret);
+    const ret = { operation: "mode" };
+    let nums = req.query.nums.split(',');
+    nums = nums.map(Number);
+    try {
+        ret.value = mode(nums)
     }
+    catch (e) {
+        next(e);
+    }
+    return res.json(ret);
 });
 
-//from the slides
+//error handler from the slides
 app.use(function (err, req, res, next) {
     // the default status is 500 Internal Server Error
     let status = err.status || 500;
---
+++ operations.js
@@ -0,0 +1,92 @@
+//from the slides
+class ExpressError extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+        // console.error(this.stack);
+    }
+}
+
+function mean(values) {
+    if (values.length !== 0) {
+        let sum = 0;
+        for (let val of values) {
+            sum += Number(val);
+            if (Number.isNaN(sum)) {
+                throw new ExpressError(`${val} is not a number`, 400);
+            }
+        }
+        return sum / values.length;
+    }
+    else {
+        throw new ExpressError("nums are required", 400);
+    }
+}
+
+function median(values) {
+    if (values.length !== 0) {
+        for (let val of values) {
+            if (Number.isNaN(Number(val))) {
+                throw new ExpressError(`${val} is not a number`, 400);
+            }
+        }
+        values.sort((x, y) => {
+            return x - y;
+        });
+        console.log(values);
+        const midpoint = Math.floor(values.length / 2);
+        if (values.length % 2 === 0) {
+            return (values[midpoint - 1] + values[midpoint]) / 2;
+        }
+        else {
+            return values[midpoint];
+        }
+    }
+    else {
+        throw new ExpressError("nums are required", 400);
+    }
+}
+
+
+function mode(values) {
+    if (values.length !== 0) {
+        for (let val of values) {
+            if (Number.isNaN(Number(val))) {
+                throw new ExpressError(`${val} is not a number`, 400);
+            }
+        }
+        if(values.length === 1){
+            return values[0];
+        }
+        let maxcount = 0;
+        let mode;
+        const counted = [];
+        for( let i = 0; i < values.length; i++){
+            if(!counted.includes(values[i])){
+                counted.push(values[i]);
+                let currcount = 1;
+                for( let j = i+1; j < values.length; j++){
+                    if(values[j] === values[i]){
+                        currcount++;
+                    }
+                    if(currcount > maxcount){
+                        mode = values[i];
+                        maxcount = currcount;
+                    }
+                }
+            }
+        }
+        return mode;
+    }
+    else {
+        throw new ExpressError("nums are required", 400);
+    }
+}
+
+module.exports = {
+    mean,
+    median,
+    mode,
+    ExpressError
+}
