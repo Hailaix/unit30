c5e1ae50fb995cc1fc214844058c6f0d89eeed0f
NHower <howernickoli@gmail.com>
2022-09-29 00:43:32 -0400

    app functional, needs tests

---
+++ 30-2/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const itemsRouter = require('./itemroutes')
+
+const app = express();
+
+app.use(express.json());
+app.use('/items', itemsRouter)
+
+//error handler from the slides
+app.use(function (err, req, res, next) {
+    // the default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
+
+    // set the status and alert the user
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
+
+app.listen(3000, function () {
+    console.log('App on port 3000');
+});
---
+++ 30-2/itemroutes.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const items = require("./fakeDb");
+const ExpressError = require("./expressError");
+
+const router = new express.Router();
+
+router.get('/', function (req, res, next) {
+    try {
+        return res.json(items);
+    }
+    catch (e) {
+        next(e);
+    }
+});
+
+router.post('/', function (req, res, next) {
+    try {
+        //extract name and price from the accepted json so we don't grab anything else
+        const name = req.body.name;
+        const price = req.body.price;
+        if (name && price) {
+            const newItem = { name, price };
+            items.push(newItem);
+            return res.json({ "added": newItem });
+        }
+        else {
+            throw new ExpressError("Name and Price Required fields", 400);
+        }
+    }
+    catch (e) {
+        next(e);
+    }
+});
+
+router.get('/:name', function (req, res, next) {
+    try {
+        const item = items.find(function (value) {
+            return value.name === req.params.name;
+        });
+        if (item) {
+            return res.json(item);
+        }
+        else {
+            throw new ExpressError("Item Not Found", 404)
+        }
+    }
+    catch (e) {
+        next(e);
+    }
+});
+
+router.patch('/:name', function (req, res, next) {
+    try {
+        const name = req.body.name;
+        const price = req.body.price;
+        const idx = items.findIndex(function (value) {
+            return value.name === req.params.name;
+        });
+        if (idx !== -1) {
+            const item = items[idx];
+            if (name && item.name !== name) {
+                item.name = name;
+            }
+            if (price && item.price !== price) {
+                item.price = price;
+            }
+            return res.json({ "updated": item });
+        }
+        else {
+            throw new ExpressError("Item Not Found", 404)
+        }
+    }
+    catch (e) {
+        next(e);
+    }
+});
+
+router.delete('/:name', function (req, res, next) {
+    try {
+        const idx = items.findIndex(function (value) {
+            return value.name === req.params.name;
+        });
+        if (idx !== -1) {
+            items.splice(idx, 1);
+            return res.json("Deleted");
+        }
+        else {
+            throw new ExpressError("Item Not Found", 404)
+        }
+    }
+    catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
